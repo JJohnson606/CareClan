af454b32c837f12c91bd674e0f56cb4d07709af1
JJohnson606 <jjdevacc@gmail.com>
2024-03-30 15:31:10 -0500

    Added Post's Approval Associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,15 +2,17 @@
 #
 # Table name: posts
 #
-#  id         :uuid             not null, primary key
-#  author_id  :uuid
-#  body       :text
-#  image      :string
-#  trusted    :boolean
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :uuid             not null, primary key
+#  author_id       :uuid
+#  body            :text
+#  image           :string
+#  trusted         :boolean
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  approvals_count :integer          default(0), not null
 #
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id' # author association
   has_many :comments, dependent: :destroy # comments association
+  has_many :approvals, dependent: :destroy # approvals association
 end
