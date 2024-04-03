4457bad758220f2a63825847e64bb24f491125ce
JJohnson606 <jjdevacc@gmail.com>
2024-04-03 13:29:45 -0500

    Added User Model Voter Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # Associations
+  acts_as_voter # for liking/disliking posts
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :medical_records_as_patient, foreign_key: 'patient_id', class_name: 'MedicalRecord', dependent: :destroy
