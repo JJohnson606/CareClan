976c5851e854c753311a697ee27db9713823c471
JJohnson606 <jjdevacc@gmail.com>
2024-04-03 15:15:17 -0500

    Generated Acts_As_Votable UUID's

---
+++ db/migrate/20240403200641_change_vote_references_to_uuid.rb
@@ -0,0 +1,15 @@
+class ChangeVoteReferencesToUuid < ActiveRecord::Migration[7.0]
+  def change
+   # Remove the columns
+   remove_column :votes, :votable_id
+   remove_column :votes, :voter_id
+
+   # Add the columns back as UUIDs
+   add_column :votes, :votable_id, :uuid, index: true
+   add_column :votes, :voter_id, :uuid, index: true
+
+   # Add new indexes as necessary
+   add_index :votes, [:votable_id, :votable_type, :vote_scope], name: 'index_votes_on_votable_and_scope'
+   add_index :votes, [:voter_id, :voter_type, :vote_scope], name: 'index_votes_on_voter_and_scope'
+  end
+end
