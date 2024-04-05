89e1aa2965af4de1ab3f64e3fab34ae2e351c1d7
JJohnson606 <jjdevacc@gmail.com>
2024-04-05 10:33:43 -0500

    Fixed Spacing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,8 +14,8 @@ class CommentsController < ApplicationController
   # GET /comments/new
   def new
     @post = Post.find(params[:post_id])
-     @parent_comment = Comment.find_by(id: params[:parent_id]) # Optional, find parent comment if present
-     @comment = Comment.new(post_id: params[:post_id], parent_id: params[:parent_id])
+    @parent_comment = Comment.find_by(id: params[:parent_id]) # Optional, find parent comment if present
+    @comment = Comment.new(post_id: params[:post_id], parent_id: params[:parent_id])
   end
 
   # GET /comments/1/edit
@@ -24,21 +24,17 @@ class CommentsController < ApplicationController
 
   # POST /comments or /comments.json
   def create
-    Rails.logger.info "Parameters: #{params.inspect}" # Log incoming parameters
     @post = Post.find(params[:comment][:post_id])
     @comment = @post.comments.build(comment_params.merge(author: current_user))
     
     if @comment.save
-      Rails.logger.info "Comment saved successfully: #{@comment.inspect}" # Log successful save
       redirect_to @post, notice: 'Comment was successfully created.'
     else
-      Rails.logger.error "Failed to save comment: #{@comment.errors.full_messages}" # Log error messages
       flash.now[:alert] = 'Failed to create comment.'
       render 'posts/show', status: :unprocessable_entity
     end
   end
-  
- 
+
   # PATCH/PUT /comments/1 or /comments/1.json
   def update
     respond_to do |format|
@@ -63,18 +59,17 @@ class CommentsController < ApplicationController
   end
 
   private
-  
+
   def set_post
     @post = Post.find(params[:comment][:post_id])
   end
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def comment_params
-      params.require(:comment).permit(:body, :post_id, :author_id)
-    end
+  def comment_params
+    params.require(:comment).permit(:body, :post_id, :author_id)
+  end
 end
+
