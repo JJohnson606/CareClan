bbf39813992b57d503d114e3ce695090ef8cbd9f
JJohnson606 <jjdevacc@gmail.com>
2024-04-06 22:03:41 -0500

    Added Medical Record Type Enum

--- app/models/medical_record.rb
+++ app/models/medical_record.rb
@@ -15,5 +15,15 @@ class MedicalRecord < ApplicationRecord
    belongs_to :patient, class_name: 'User', foreign_key: 'patient_id'
    belongs_to :creator, class_name: 'User', foreign_key: 'created_by_id'
    has_many :posts
+
+   enum record_type: {
+    diagnosis: 'Diagnosis',
+    treatment_plan: 'Treatment Plan',
+    prescription: 'Prescription',
+    lab_results: 'Lab Results',
+    imaging: 'Imaging',
+    progress_notes: 'Progress Notes',
+    surgical_reports: 'Surgical Reports',
+    vaccination_records: 'Vaccination Records'
+  }
  end
-  
