49b3d91bfb157c61cdab830b575a48f88abc727d
JJohnson606 <jjdevacc@gmail.com>
2024-04-07 17:39:01 -0500

    Created Routes For Medical Records Partials

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   end
   devise_for :users
   get 'dashboard', to: 'dashboard#index', as: 'dashboard'
+  get '/medical_records/partials/:record_type', to: 'medical_records#load_partial', as: 'load_medical_record_partial'
+  get '/medical_records/fields_partials/:record_type', to: 'medical_records#fields_partial', as: 'fields_partial'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
