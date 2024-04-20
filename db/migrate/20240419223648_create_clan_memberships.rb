740e35fcb537705bc8812e36606e7dd2e9531d7a
JJohnson606 <jjdevacc@gmail.com>
2024-04-20 12:56:34 -0500

    Migrated Family Clan Tables

---
+++ db/migrate/20240419223648_create_clan_memberships.rb
@@ -0,0 +1,11 @@
+class CreateClanMemberships < ActiveRecord::Migration[7.0]
+  def change
+    create_table :clan_memberships, id: :uuid do |t|
+      t.references :clan, null: false, foreign_key: true, type: :uuid
+      t.references :user, null: false, foreign_key: true, type: :uuid
+      t.string :role, default: "member", null: false
+
+      t.timestamps
+    end
+  end
+end
