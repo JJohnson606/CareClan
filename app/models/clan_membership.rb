62d760ba5a59aa7f9ecdace6d16445fb82bc3ad8
JJohnson606 <jjdevacc@gmail.com>
2024-04-20 12:56:54 -0500

    Created Clan Membership Model

---
+++ app/models/clan_membership.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: clan_memberships
+#
+#  id         :uuid             not null, primary key
+#  clan_id    :uuid             not null
+#  user_id    :uuid             not null
+#  role       :string           default("member"), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class ClanMembership < ApplicationRecord
+    belongs_to :clan
+    belongs_to :user
+  end
+  
