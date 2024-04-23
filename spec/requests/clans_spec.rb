086963481828990d2df5170d78e6fd66f1590cf7
JJohnson606 <jjdevacc@gmail.com>
2024-04-23 13:25:42 -0500

    Spec files for Clan_Membeship, Clans, Clans Helpers.

---
+++ spec/requests/clans_spec.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: clans
+#
+#  id          :uuid             not null, primary key
+#  name        :string           not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe "Clans", type: :request do
+  describe "GET /index" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end
