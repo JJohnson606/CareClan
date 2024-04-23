be1b9e50904b390c481a14bba433b3864dd7ecf2
JJohnson606 <jjdevacc@gmail.com>
2024-04-23 13:20:20 -0500

    Created Clan User Helper Methods In Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user_can_edit?, :current_user_is_admin?
+
+  private
+
+  def current_user_can_edit?(membership)
+    current_user == membership.user || current_user_is_admin?(membership.clan)
+  end
+
+  def current_user_is_admin?(clan)
+    clan.clan_memberships.exists?(user_id: current_user.id, role: 'admin')
+  end
 end
---
+++ app/helpers/clan_memberships_helper.rb
@@ -0,0 +1,2 @@
+module ClanMembershipsHelper
+end
