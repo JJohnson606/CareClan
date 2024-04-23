4d0377d89daa7bcc0f2e7133ffaa347c363229e1
JJohnson606 <jjdevacc@gmail.com>
2024-04-22 20:29:42 -0500

    Mounted Sidekiq Route For Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+require "sidekiq/web"
 Rails.application.routes.draw do
   get 'home/chartview'
   root "welcome#index"
@@ -32,4 +33,5 @@ Rails.application.routes.draw do
 
   get 'dashboard', to: 'dashboard#index', as: 'dashboard'
 
+  mount Sidekiq::Web => '/sidekiq'
 end
