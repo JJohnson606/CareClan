bdbc6fd3851fa08b5cdf493824be94cf9b0eee50
JJohnson606 <jjdevacc@gmail.com>
2024-04-23 21:45:38 -0500

    Created New Post Notification Job

---
+++ app/jobs/new_post_notification_job.rb
@@ -0,0 +1,11 @@
+class NewPostNotificationJob < ApplicationJob
+  queue_as :default
+ 
+  def perform(post)
+    post.author.clans.each do |clan|
+      clan.users.each do |user|
+        UserMailer.new_post_notification(user, post).deliver_later
+      end
+    end
+  end
+end
