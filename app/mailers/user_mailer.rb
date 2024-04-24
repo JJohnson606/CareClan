63adaf50a9c1660f2530fe68e67affccc603a574
JJohnson606 <jjdevacc@gmail.com>
2024-04-23 21:46:58 -0500

    Added New Post Notification Method To User Mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,5 +5,11 @@ class UserMailer < ApplicationMailer
       @message = message
       mail(to: @user.email, subject: 'New Comment on Your Post')
     end
+ 
+    def new_post_notification(user, post)
+      @user = user
+      @post = post
+      mail(to: @user.email, subject: 'New Post Published')
+    end
   end
   
