5dab388fcc7c83a3bb7d99164c023fabab3ee658
JJohnson606 <jjdevacc@gmail.com>
2024-04-25 14:28:22 -0500

    Optimized Comments Index and Show. || Attempted Comment Create Javascripts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,12 @@ class CommentsController < ApplicationController
 
   # GET /comments or /comments.json
   def index
-    @comments = Comment.all
+    @comments = Comment.includes(:author, replies: :author).all
   end
 
   # GET /comments/1 or /comments/1.json
   def show
+    @comment = Comment.includes(replies: :author).find(params[:id])
   end
 
   # GET /comments/new
@@ -25,12 +26,20 @@ class CommentsController < ApplicationController
   def create
     @comment = @post.comments.create(comment_params.merge(author: current_user))
     if @comment.save
-      # Trigger notification after successful save
-      NewCommentNotifier.with(post: @post, user: current_user, message: "New comment on your post!").deliver(current_user)
-      redirect_to @post, notice: 'Comment was successfully created.'
+      Rails.logger.debug "Processing as: #{request.format.to_sym}"
+      respond_to do |format|
+        format.html { redirect_to @post, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @comment }
+        format.turbo_stream
+        format.js
+      end
     else
-      flash.now[:alert] = 'Failed to create comment.'
-      render 'posts/show', status: :unprocessable_entity
+      respond_to do |format|
+        format.html { render 'posts/show', status: :unprocessable_entity }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("comment_form", partial: "comments/form", locals: { comment: @comment }) }
+        format.js
+      end
     end
   end
 
@@ -92,7 +101,7 @@ end
   end
 
   def comment_params
-    params.require(:comment).permit(:body, :parent_id)
+    params.require(:comment).permit(:body, :parent_id, :author_id)
   end
 end
 
