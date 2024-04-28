a72d2461f183ffbb30ae0fd524ac7a5fc3fe0a4b
Jason Johnson <jjdevacc@gmail.com>
2024-04-28 17:31:24 -0500

    Added Post Voters and and Update Cache To Votable Module.

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -3,7 +3,29 @@ module Votable
   extend ActiveSupport::Concern
 
   included do
-    acts_as_votable cacheable_strategy: :update_columns
+    acts_as_votable
+
+    def voters_up
+      User.joins(:votes).where(votes: { votable: self, vote_flag: true }).includes(:profile_picture_attachment)
+    end
+
+    def voters_down
+      User.joins(:votes).where(votes: { votable: self, vote_flag: false }).includes(:profile_picture_attachment)
+    end
+
+    def update_vote_cache
+      upvotes = get_upvotes.size
+      downvotes = get_downvotes.size
+      total_votes = upvotes + downvotes
+      vote_diff = (upvotes - downvotes).abs
+
+      update_columns(
+        cached_votes_up: upvotes,
+        cached_votes_down: downvotes,
+        cached_votes_total: total_votes,
+        cached_vote_diff: vote_diff
+      )
+    end
 
     def approval_rating
       total_votes = votes_for.size
