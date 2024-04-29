2302459c37c72df54bcffcee99ab7055fa60fc07
Jason Johnson <jjdevacc@gmail.com>
2024-04-28 20:41:25 -0500

    Refactored Medical Record Controller Trusted Params

--- app/controllers/medical_records_controller.rb
+++ app/controllers/medical_records_controller.rb
@@ -15,15 +15,14 @@ class MedicalRecordsController < ApplicationController
     else
       @q = MedicalRecord.ransack(params[:q])
     end
-  
+
     # Preload patients and their profile pictures as well as creators and their profile pictures
     @medical_records = @q.result(distinct: true)
-    .includes(patient: [profile_picture_attachment: :blob], 
+    .includes(patient: [profile_picture_attachment: :blob],
               creator: [profile_picture_attachment: :blob])
     .page(params[:page])
 
   end
-  
 
   # GET /medical_records/1 or /medical_records/1.json
   def show
@@ -80,16 +79,16 @@ class MedicalRecordsController < ApplicationController
       format.json { head :no_content }
     end
   end
-  
+
   def load_partial
     # Ensure this 'form' object is correctly initialized as needed for your partials
     render partial: "medical_records/partials/#{params[:record_type]}", locals: { medical_record: @medical_record }
   end
-  
+
   def fields_partial
     render partial: "medical_records/partials/#{params[:record_type]}_fields", locals: { form: form_builder }
   end
-  
+
   def form_builder
     # This method should return a form builder instance that can be used in the partial
     # For example, if using simple_form, you might do something like this:
@@ -102,73 +101,28 @@ class MedicalRecordsController < ApplicationController
   def set_medical_record
     @medical_record = MedicalRecord.find(params[:id])
   end
-  
+
   # Only allow a list of trusted parameters through.
   def medical_record_params
     params.require(:medical_record).permit(:patient_id, :record_type, :record_date, :created_by_id, images: []).tap do |permitted_params|
-      case permitted_params[:record_type]
-      when 'diagnosis'
-        notes_data = {
-          condition: params[:medical_record][:condition],
-          symptoms: params[:medical_record][:symptoms],
-          recommended_treatment: params[:medical_record][:recommended_treatment]
-        }
-        permitted_params[:notes] = notes_data.to_json
-  
-      when 'imaging'
-        notes_data = {
-          type: params[:medical_record][:type],
-          findings: params[:medical_record][:findings],
-          interpretation: params[:medical_record][:interpretation]
-        }
-        permitted_params[:notes] = notes_data.to_json
-  
-      when 'lab_results'
-        # Assuming lab results are complex and need special handling
-        # You would need to serialize each lab test and its results
-  
-      when 'prescription'
-        notes_data = {
-          medication_name: params[:medical_record][:medication_name],
-          dosage: params[:medical_record][:dosage],
-          duration: params[:medical_record][:duration],
-          purpose: params[:medical_record][:purpose]
-        }
-        permitted_params[:notes] = notes_data.to_json
-  
-      when 'progress_notes'
-        notes_data = {
-          date: params[:medical_record][:date],
-          note: params[:medical_record][:note]
-        }
-        permitted_params[:notes] = notes_data.to_json
-  
-      when 'surgical_reports'
-        notes_data = {
-          procedure: params[:medical_record][:procedure],
-          date: params[:medical_record][:date],
-          findings: params[:medical_record][:findings],
-          outcome: params[:medical_record][:outcome]
-        }
-        permitted_params[:notes] = notes_data.to_json
-  
-      when 'treatment_plan'
-        notes_data = {
-          treatment: params[:medical_record][:treatment],
-          duration: params[:medical_record][:duration],
-          follow_up: params[:medical_record][:follow_up]
-        }
-        permitted_params[:notes] = notes_data.to_json
-  
-      when 'vaccination_records'
-        notes_data = {
-          vaccine: params[:medical_record][:vaccine],
-          date: params[:medical_record][:date],
-          lot_number: params[:medical_record][:lot_number],
-          site: params[:medical_record][:site]
-        }
-        permitted_params[:notes] = notes_data.to_json
-      end
+      permitted_params[:notes] = case permitted_params[:record_type]
+                                 when 'diagnosis'
+                                   RecordTypes::DiagnosisNotes.permitted_params(params)
+                                 when 'treatment_plan'
+                                   RecordTypes::TreatmentPlanNotes.permitted_params(params)
+                                 when 'prescription'
+                                   RecordTypes::PrescriptionNotes.permitted_params(params)
+                                 when 'imaging'
+                                   RecordTypes::ImagingNotes.permitted_params(params)
+                                 when 'progress_notes'
+                                   RecordTypes::ProgressNotesNotes.permitted_params(params)
+                                 when 'surgical_reports'
+                                   RecordTypes::SurgicalReportsNotes.permitted_params(params)
+                                 when 'vaccination_records'
+                                   RecordTypes::VaccinationRecordsNotes.permitted_params(params)
+                                 when 'lab_results'
+                                   RecordTypes::LabResultsNotes.permitted_params(params)
+                                 end
     end
   end
 end
