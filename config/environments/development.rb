a0638eaf7c0305b0e312206bcce0cc80adbfdb83
Jason Johnson <jjdevacc@gmail.com>
2024-04-28 20:04:23 -0500

    Set Bullet.rails_logger to True

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
     Bullet.alert         = false
     Bullet.bullet_logger = false
     Bullet.console       = false
-    Bullet.rails_logger  = false
+    Bullet.rails_logger  = true
     Bullet.add_footer    = false
   end
 
