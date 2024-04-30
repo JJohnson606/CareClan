ef89623584cf5be4c9daeccf4e2b4102a059ab87
Jason Johnson <jjdevacc@gmail.com>
2024-04-30 12:48:16 -0500

    Deleted Additional Tailwind Themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,20 +34,6 @@ module.exports = {
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
     themes: [
-      {
-        // Custom theme named 'mytheme'
-        apple: {
-          "primary": "#3abf23",    // Map '500' from the apple color scheme to primary
-          "secondary": "#b3f2a4",  // Map '200' from the apple color scheme to secondary
-          "accent": "#83e86e",     // Map '300' from the apple color scheme to accent
-          "neutral": "#3d4451",    // You can choose a neutral color or use the one from apple
-          "base-100": "#edfce9",   // Map '50' from the apple color scheme to base-100
-          // Add more custom colors as needed...
-        },
-      },
-      
-      "dark",
-      "cupcake",
       "emerald",
     ],
    
