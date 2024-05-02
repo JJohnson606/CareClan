e230f642ebc34e2d1363b664f1a1840fd1d9a136
Jason Johnson <jjdevacc@gmail.com>
2024-05-02 17:31:59 -0500

    Deleted Redundant Comment Mailer

--- app/mailers/comment_mailer.rb
+++
@@ -1,7 +0,0 @@
-class CommentMailer < ApplicationMailer
-    def new_comment(post, user)
-      @post = post
-      @user = user
-      mail(to: @user.email, subject: 'New Comment on Your Post')
-    end
-  end
