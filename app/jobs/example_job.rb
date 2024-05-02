a85d6a1146309510a4e74965008754fb10189eab
Jason Johnson <jjdevacc@gmail.com>
2024-05-02 16:51:53 -0500

    Removed Sidekiq form Example Worker and Job

--- app/jobs/example_job.rb
+++ app/jobs/example_job.rb
@@ -1,9 +1,3 @@
 # app/jobs/example_job.rb
 class ExampleJob
-    include Sidekiq::Worker
-  
-    def perform(*args)
-      puts "Job is running with arguments: #{args.inspect}"
-    end
-  end
-  
+end
