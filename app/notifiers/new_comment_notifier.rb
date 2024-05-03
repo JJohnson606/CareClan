829f84d7725b4819c348f49f57fa757b3c3ac64d
Jason Johnson <jjdevacc@gmail.com>
2024-05-02 19:25:46 -0500

    Changed Comment Notifier Argument Method To Comment.

--- app/notifiers/new_comment_notifier.rb
+++ app/notifiers/new_comment_notifier.rb
@@ -1,12 +1,14 @@
 class NewCommentNotifier < Noticed::Event
-  deliver_by :email, mailer: "UserMailer", method: :new_comment_notification, if: -> { user.allow_email_notifications? }
-  param :post, :user, :message
+  deliver_by :email, mailer: "UserMailer", method: :new_comment_notification
+  required_param :post
+  required_param :user
+  required_param :comment
 
   def to_mailer
     {
       user: params[:user],
       post: params[:post],
-      message: params[:message]
+      comment: params[:comment]
     }
   end
 end
