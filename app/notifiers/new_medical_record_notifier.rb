4f8220ba2c905c7763cf5f6272579ce38cceee84
Jason Johnson <jjdevacc@gmail.com>
2024-05-02 19:26:27 -0500

    Added User as a Required Param For New Medical Record Notification

--- app/notifiers/new_medical_record_notifier.rb
+++ app/notifiers/new_medical_record_notifier.rb
@@ -1,6 +1,7 @@
 class NewMedicalRecordNotifier < Noticed::Event
   deliver_by :email, mailer: "UserMailer", method: :new_medical_record_notification
 
+  required_param :user
   required_param :medical_record
 
   def to_mailer
