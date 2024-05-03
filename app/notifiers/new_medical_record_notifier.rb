d8b726efaf8c08b230283508e1dfd4f99713ed8c
Jason Johnson <jjdevacc@gmail.com>
2024-05-02 19:22:42 -0500

    Removed Database Methods From Medical Record Notifier

--- app/notifiers/new_medical_record_notifier.rb
+++ app/notifiers/new_medical_record_notifier.rb
@@ -1,15 +1,7 @@
 class NewMedicalRecordNotifier < Noticed::Event
-  deliver_by :database  # logs to the database
   deliver_by :email, mailer: "UserMailer", method: :new_medical_record_notification
 
   required_param :medical_record
-
-  def to_database
-    {
-      title: "New Medical Record Added",
-      text: "A new medical record for #{params[:medical_record].patient.name} has been added.",
-    }
-  end
 
   def to_mailer
     UserMailer.new_medical_record_notification(params[:medical_record].patient, params[:medical_record])
