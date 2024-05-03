773511a5f12969e1354a804940974ce7c525fa63
Jason Johnson <jjdevacc@gmail.com>
2024-05-02 19:23:38 -0500

    Removed User Argument From New Medical Record Notification Method

--- app/jobs/new_medical_record_notification_job.rb
+++ app/jobs/new_medical_record_notification_job.rb
@@ -1,7 +1,7 @@
 class NewMedicalRecordNotificationJob < ApplicationJob
   queue_as :default
 
-  def perform(user, medical_record)
+  def perform(medical_record)
     medical_record.patient.clans.each do |clan|
       clan.users.each do |user|
         UserMailer.new_medical_record_notification(user, medical_record).deliver_later
