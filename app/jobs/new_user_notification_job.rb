a271772947ed9ca1cf006d2924d0877d6e99433a
Jason Johnson <jjdevacc@gmail.com>
2024-05-09 18:09:15 -0500

    Created New User Notification Job for New User Welcome and New Clan Member Notificaiton.

---
+++ app/jobs/new_user_notification_job.rb
@@ -0,0 +1,18 @@
+class NewUserNotificationJob < ApplicationJob
+  queue_as :default
+
+  def perform(user)
+    # Send welcome email to the new user
+    NewUserWelcomeNotifier.with(user:).deliver_later
+
+    # Notify clan members about the new user, excluding the user themselves
+    user.clans.each do |clan|
+      clan_members = clan.users.where.not(id: user.id)
+      next unless clan_members.any?
+
+      clan_members.each do |member|
+        NewUserClanNotifier.with(user:, clan:).deliver_later
+      end
+    end
+  end
+end
