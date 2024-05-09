51ea0b779749afb175be0363e1e4974ea7dfb124
Jason Johnson <jjdevacc@gmail.com>
2024-05-09 18:01:49 -0500

    Formatted New Post Notifier

--- app/notifiers/new_post_notifier.rb
+++ app/notifiers/new_post_notifier.rb
@@ -1,12 +1,12 @@
 class NewPostNotifier < Noticed::Event
-  deliver_by :email, mailer: "UserMailer", method: :new_post_notification
+  deliver_by :email, mailer: 'UserMailer', method: :new_post_notification
   required_param :user
   required_param :post
 
   def to_mailer
     {
       user: params[:user],
-      post: params[:post],
+      post: params[:post]
     }
   end
 end
