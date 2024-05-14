362ad0aaef237e6a4d5950081bdb234da0c54563
Jason Johnson <jjdevacc@gmail.com>
2024-05-14 12:05:58 -0500

    Created User Not Authorized Method In Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,9 @@ class ApplicationController < ActionController::Base
   def current_user_is_admin?(clan)
     clan.clan_memberships.exists?(user_id: current_user.id, role: 'admin')
   end
+
+  def user_not_authorized
+    flash[:alert] = 'You are not authorized to perform this action.'
+    redirect_to(request.referer || root_path)
+  end
 end
